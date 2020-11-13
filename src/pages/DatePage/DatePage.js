74f3e48a865eef931c69060b0d669cec35371d65
Serzh108 <serzh@gabris.kiev.ua>
2020-11-14 00:51:52 +0200

    first step

--- src/pages/DatePage/DatePage.js
+++ src/pages/DatePage/DatePage.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import styles from './DatePage.module.css';
 
 export default function DatePage() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const clickHandler = () => {
+    console.log('startDate = ', startDate.toLocaleString());
+    const formatedData = startDate.toISOString().slice(0, 10);
+    console.log('formatedData = ', formatedData);
+  };
+
   return (
     <div className={styles.container}>
       DatePage
@@ -9,6 +19,12 @@ export default function DatePage() {
         Для получения списка сериалов, пожалуйста, выберите необходимый месяц и
         день.
       </p>
+      <DatePicker
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        onClick={clickHandler()}
+        inline
+      />
     </div>
   );
 }
--- src/pages/SeriesPage/SeriesPage.js
+++ src/pages/SeriesPage/SeriesPage.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from './SeriesPage.module.css';
 
+const initialState = {
+  mediumImg: '',
+  originalImg: '',
+};
+
 export default function SeriesPage() {
+  const [state, setState] = useState(initialState);
+
+  const getSeries = async () => {
+    const url = 'http://api.tvmaze.com/schedule?country=US&date=2020-11-12';
+    try {
+      const planetsArray = await axios.get(url);
+      console.log('planetsArray = ', planetsArray.data);
+      console.log('planetsArray = ', planetsArray.data[0].show.image.medium);
+      setState({
+        mediumImg: planetsArray.data[0].show.image.medium,
+        originalImg: planetsArray.data[0].show.image.original,
+      });
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  useEffect(() => {
+    getSeries();
+  }, []);
+
   return (
     <div className={styles.container}>
       SeriesPage
-      <p>
-        Для получения списка сериалов, пожалуйста, выберите необходимый месяц и
-        день.
-      </p>
+      <p>mediumImg:</p>
+      <img src={state.mediumImg} alt="images" />
+      <p>originalImg:</p>
+      <img src={state.originalImg} alt="images" />
     </div>
   );
 }
